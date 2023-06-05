c8f0f17aa599dc2094f34e9e6111f0b1767c40b5
jihenJN <jihenjihen23@gmail.com>
2023-06-06 00:50:31 +0100

    first commit

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
+
+
+import { Login } from 'src/app/models/login';
+import { Account } from '../models/account';
+import { AccountService } from './account.service';
+import { AuthServerProvider } from './auth-jwt.service';
+
+@Injectable({ providedIn: 'root' })
+export class LoginService {
+  constructor(private accountService: AccountService, private authServerProvider: AuthServerProvider) {}
+
+  login(credentials: Login): Observable<Account | null> {
+    return this.authServerProvider.login(credentials).pipe(mergeMap(() => this.accountService.identity(true)));
+  }
+
+  logout(): void {
+    this.authServerProvider.logout().subscribe({ complete: () => this.accountService.authenticate(null) });
+  }
+}
